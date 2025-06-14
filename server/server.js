0e33b91fa0101ae9c953ffa76d67b5446afb1dab
Burhan Ahmad <burhan7554@gmail.com>
2025-06-14 19:03:12 +0500

    cors

--- server/server.js
+++ server/server.js
@@ -19,11 +19,11 @@ const uploadRoutes = require('./Routes/uploadImageRoutes')
 ConnectDB();
 
 app.use(express.static('public'));
-app.use(cors({
-    origin: 'http://localhost:5173',
-    methods: ['GET', 'POST', 'PUT', 'DELETE'],
-    credentials: true,
-}));
+// app.use(cors({
+//     origin: 'http://localhost:5173',
+//     methods: ['GET', 'POST', 'PUT', 'DELETE'],
+//     credentials: true,
+// }));
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
